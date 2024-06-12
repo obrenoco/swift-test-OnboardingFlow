392393ffa671efbb9d1cdb0f39b9bc1b5bcdb5d0
Breno <bsromeiro@gmail.com>
2024-06-12 17:16:35 -0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Breno Romeiro on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
